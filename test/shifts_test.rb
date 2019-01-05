cf7a8080e5de68411f5c6c8b09d5118ce2b1ed2b
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-01-05 14:46:22 -0700

    Added setup/test it exists

--- test/shifts_test.rb
+++ test/shifts_test.rb
@@ -3,4 +3,12 @@ require './lib/shifts'
 
 class ShiftsTest < Minitest::Test
 
+  def setup
+    @shifts = Shifts.new
+  end
+
+  def test_it_exists
+    assert_instance_of Shifts, @shifts
+  end
+
 end
