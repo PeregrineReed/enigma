9da8808085fddd500694e9fd3e908c70364ecfd7
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-01-05 16:34:01 -0700

    Removed optional arguments test

--- test/shifts_test.rb
+++ test/shifts_test.rb
@@ -4,17 +4,11 @@ require './lib/shifts'
 class ShiftsTest < Minitest::Test
 
   def setup
-    @shifts = Shifts.new
+    @shifts = Shifts.new("00001", "200792")
   end
 
   def test_it_exists
     assert_instance_of Shifts, @shifts
-  end
-
-  def test_it_can_take_arguments
-    shifts = Shifts.new(key: "30030", date: "050119")
-
-    assert_instance_of Shifts, shifts
   end
 
   def test_it_has_a_key
