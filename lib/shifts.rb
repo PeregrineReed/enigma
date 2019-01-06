631a21fe9d58ed830f63d2a50baeaaa70a53563d
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-01-05 19:50:48 -0700

    Updated method for encryption

--- lib/shifts.rb
+++ lib/shifts.rb
@@ -10,6 +10,14 @@ class Shifts
     @date = date
   end
 
+  def self.for_encryption(key, date)
+    shifts = keys(key).zip(offsets(date))
+    set = shifts.map do |pair|
+      pair[0].to_i + pair[1].to_i
+    end.flatten
+    Shifts.new(set, key, date)
+  end
+
   def self.offsets(date)
     date_squared = date.to_i ** 2
     offsets = date_squared.to_s[-4..-1]
@@ -27,13 +35,6 @@ class Shifts
   def self.keys(key)
     make_key_pairs(key).map do |pair|
       pair.join
-    end.flatten
-  end
-
-  def make_shifts
-    shifts = make_keys.zip(make_offsets)
-    shifts = shifts.map do |pair|
-      pair[0].to_i + pair[1].to_i
     end.flatten
   end
 
