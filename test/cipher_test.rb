01cc23545e84b8aca7a5db3b86b6b3c98ef8cc9a
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-01-06 18:50:21 -0700

    changed instance name in test

--- test/cipher_test.rb
+++ test/cipher_test.rb
@@ -5,25 +5,25 @@ require './lib/cipher'
 class CipherTest < Minitest::Test
 
   def setup
-    @encryption = Cipher.new('message', '00001', '200792')
+    @cipher = Cipher.new('message', '00001', '200792')
   end
 
   def test_it_exists
-    assert_instance_of Cipher, @encryption
+    assert_instance_of Cipher, @cipher
   end
 
   def test_it_has_a_message
-    assert_equal "message", @encryption.message
+    assert_equal "message", @cipher.message
   end
 
   def test_it_has_shifts
-    assert_equal [7, 2, 6, 5], @encryption.shifts
+    assert_equal [7, 2, 6, 5], @cipher.shifts
   end
 
   def test_it_can_split_its_message
     expected = ['m', 'e', 's', 's', 'a', 'g', 'e']
 
-    assert_equal expected, @encryption.split_message
+    assert_equal expected, @cipher.split_message
   end
 
 end
