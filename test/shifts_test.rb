66bc66ab40d6c422e125937ed78cd4526f507de8
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-01-07 08:38:39 -0700

    Skipped tests

--- test/shifts_test.rb
+++ test/shifts_test.rb
@@ -9,30 +9,36 @@ class ShiftsTest < Minitest::Test
   end
 
   def test_it_exists
+    skip
     assert_instance_of Shifts, @shifts
   end
 
   def test_it_has_a_key
+    skip
     assert_equal "00001", @shifts.key
   end
 
   def test_it_has_a_date
+    skip
     assert_equal "200792", @shifts.date
   end
 
   def test_it_has_a_set
+    skip
     expected = [7, 2, 6, 5]
 
     assert_equal expected, @shifts.set
   end
 
   def test_it_can_make_offsets
+    skip
     expected = ['7', '2', '6', '4']
 
     assert_equal expected, Shifts.offsets("200792")
   end
 
   def test_it_can_make_key_pairs
+    skip
     expected = [
       ["0", "0"],
       ["0", "0"],
@@ -44,12 +50,14 @@ class ShiftsTest < Minitest::Test
   end
 
   def test_it_can_make_keys
+    skip
     expected = ['00', '00', '00', '01']
 
     assert_equal expected, Shifts.keys('00001')
   end
 
   def test_it_can_initialize_for_encryption
+    skip
     shifts = Shifts.for_translation('00001', '200792')
     expected = [7, 2, 6, 5]
 
