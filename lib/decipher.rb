b222df84e9bca89c2cd8a53664e0c7dac8862a26
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-01-07 09:59:06 -0700

    Added method to decipher sets of 4

--- lib/decipher.rb
+++ lib/decipher.rb
@@ -1,3 +1,11 @@
 module Decipher
 
+  def decrypt_4_digits(four)
+    four.map do |letter|
+      shift = @shifts[four.index(letter)]
+      index = characters.index(letter)
+      characters.rotate(-shift).values_at(index)
+    end.flatten.join
+  end
+
 end
