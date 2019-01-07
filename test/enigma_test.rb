80b93e4877af1825acf4ce066633ebe0d485386d
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-01-07 16:59:07 -0700

    Rewrote tests to generate key and date

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -15,32 +15,6 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, @enigma
   end
 
-  def test_it_has_a_message
-    assert_equal 'message', @enigma.message
-  end
-
-  def test_it_has_a_key
-    assert_equal '00001', @enigma.key
-  end
-
-  def test_it_has_a_date
-    assert_equal '200792', @enigma.date
-  end
-
-  def test_it_can_generate_todays_date
-    skip
-    enigma = Enigma.new("testing testing", "12345")
-    expected = Date.today.strftime('d','m','y')
-    assert_equal expected, enigma.date
-  end
-
-  def test_it_can_generate_a_key
-    skip
-    enigma = Enigma.new("testing testing")
-    actual = /\d\d\d\d\d/ =~ enigma.key
-    assert_equal true, actual
-  end
-
   def test_it_can_encrypt
     expected = {
       encryption: 'tgyyhik',
@@ -50,6 +24,20 @@ class EnigmaTest < Minitest::Test
     actual = @enigma.encrypt('message', '00001', '200792')
 
     assert_equal expected, actual
+  end
+
+  def test_it_can_generate_todays_date_for_encrypting
+    skip
+    expected = Date.today.strftime('d','m','y')
+    actual = @enigma.encrypt("testing testing", "12345")
+    assert_equal expected, actual
+  end
+
+  def test_it_can_generate_a_key_for_encrypting
+    skip
+    enigma = @enigma.encrypt("testing testing")
+    actual = /\d\d\d\d\d/ =~ enigma.key
+    assert_equal true, actual
   end
 
 end
