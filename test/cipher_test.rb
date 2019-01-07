49905d65a5c339b5e6ed46ab2f1fe1b6e7f1dd93
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-01-07 09:41:28 -0700

    Added test for module cipher

--- test/cipher_test.rb
+++ test/cipher_test.rb
@@ -1,7 +1,7 @@
 require './test/test_helper'
 require './lib/cipher'
 
-class Cipher
+class CipherClass
   include Cipher
 
   def initialize(shifts)
@@ -12,7 +12,15 @@ end
 class CipherTest < Minitest::Test
 
   def setup
-    @cipher = Cipher.new([7,2,6,5])
+    @cipher = CipherClass.new([7,2,6,5])
+  end
+
+  def test_it_exists
+    actual = CipherClass.included_modules.any? do |mod|
+      mod == Cipher
+    end
+
+    assert_equal true, actual
   end
 
 end
