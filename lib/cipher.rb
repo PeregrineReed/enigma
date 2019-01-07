3049fd3294076edb0f6cc580f4e20d29fdfff2b1
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-01-06 19:03:10 -0700

    Added method to encrypt 4 digits

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -12,4 +12,12 @@ class Cipher
     message.split('')
   end
 
+  def encrypt_4_digits(four)
+    four.map do |letter|
+      shift = @shifts[four.index(letter)]
+      index = characters.index(letter)
+      characters.rotate(shift).values_at(index)
+    end.flatten.join
+  end
+
 end
