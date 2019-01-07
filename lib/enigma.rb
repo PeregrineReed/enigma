ac259653de614072c87f98e50465baee254619c1
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-01-07 16:39:35 -0700

    Added date to attr_reader

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,10 +1,12 @@
 class Enigma
 
   attr_reader :message,
-              :key
+              :key,
+              :date
 
   def initialize(message, key, date)
     @message = message
     @key = key
+    @date = date
   end
 end
