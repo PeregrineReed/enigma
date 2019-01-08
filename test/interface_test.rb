de38fa96f2ebc0752f473ae046cbb66edb5a14ec
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-01-07 20:45:13 -0700

    Added test for starting without commands

--- test/interface_test.rb
+++ test/interface_test.rb
@@ -4,12 +4,20 @@ require './lib/interface'
 class InterfaceTest < Minitest::Test
 
   def setup
-    test_class = mock
-    @interface = Interface.new(test_class)
+    @test_class = mock
+    @interface = Interface.new(@test_class)
   end
 
   def test_it_exists
     assert_instance_of Interface, @interface
+  end
+
+  def test_it_has_a_subject
+    assert_equal @test_class, @interface.subject
+  end
+
+  def test_it_starts_with_no_commands
+    assert_equal [], @interface.commands
   end
 
 end
