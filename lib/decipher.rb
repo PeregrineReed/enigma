bdd6a2a0d45b04c50b7474b8e00ea7f44c01568e
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-01-08 13:52:41 -0700

    Added method to decrypt digits

--- lib/decipher.rb
+++ lib/decipher.rb
@@ -12,20 +12,28 @@ class Decipher
     @message = decrypt
   end
 
-  def decrypt_4_digits(four)
-    four.map do |letter|
-      shift = @shifts[four.index(letter)]
-      index = characters.index(letter)
-      characters.rotate(-shift).values_at(index)
-    end.flatten.join
-  end
-
   def decrypt
     code = []
     split_input.each_slice(4) do |slice|
       code << decrypt_4_digits(slice)
     end
     code.join
+  end
+
+  def decrypt_4_digits(four)
+    four.map do |letter|
+      shift = @shifts[four.index(letter)]
+      index = characters.index(letter)
+      decrypt_digit(letter, shift, index)
+    end.flatten.join
+  end
+
+  def decrypt_digit(letter, shift, index)
+    if characters.include?(letter)
+      characters.rotate(-shift).values_at(index)
+    else
+      letter
+    end
   end
 
 end
