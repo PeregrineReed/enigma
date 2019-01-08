e27b7589ce0b30f2d02c51478e48a71cc0db7b3b
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-01-08 08:39:50 -0700

    Completed encrypt runner

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -10,3 +10,6 @@ require 'pry'
 enigma = Enigma.new
 source = enigma.read_file.downcase
 alteration = enigma.encrypt(source)
+
+enigma.write_file(alteration[:encryption])
+puts "Created '#{ARGV[1]}' with the key #{alteration[:key]} and date #{alteration[:date]}"
