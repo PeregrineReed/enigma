dc612562118725ea313bf3ee3b5a606fc34e2bd4
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-01-08 15:53:25 -0700

    Added conditional for key and date

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -5,3 +5,12 @@ require './lib/decipher'
 require './lib/interface'
 require './lib/enigma'
 require 'date'
+
+enigma = Enigma.new
+source = enigma.read_file.downcase
+
+if !!(ARGV[2] =~ /\d{5}/) && !!(ARGV[3] =~ /\d{6}/)
+  alteration = enigma.decrypt(source, ARGV[2].to_s, ARGV[3].to_s)
+elsif !!(ARGV[2] =~ /\d{5}/)
+  alteration = enigma.decrypt(source, ARGV[2].to_s)
+end
