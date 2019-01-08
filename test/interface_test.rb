73f2192208162a08aba9841963c2569fad2f295d
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-01-07 20:53:31 -0700

    Removed subject argument

--- test/interface_test.rb
+++ test/interface_test.rb
@@ -4,16 +4,11 @@ require './lib/interface'
 class InterfaceTest < Minitest::Test
 
   def setup
-    @test_class = mock
-    @interface = Interface.new(@test_class)
+    @interface = Interface.new
   end
 
   def test_it_exists
     assert_instance_of Interface, @interface
-  end
-
-  def test_it_has_a_subject
-    assert_equal @test_class, @interface.subject
   end
 
   def test_it_starts_with_no_commands
