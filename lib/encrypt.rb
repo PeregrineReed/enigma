a023e727372905d1602a1ec061dea1742628a96b
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-01-08 18:00:10 -0700

    Removed requires, added functionality for key/date

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,15 +1,15 @@
-require './lib/shifts'
-require './lib/characters'
-require './lib/cipher'
-require './lib/decipher'
-require './lib/interface'
 require './lib/enigma'
-require 'date'
-require 'pry'
 
 enigma = Enigma.new
 source = enigma.read_file.downcase
-alteration = enigma.encrypt(source)
+
+if !!(ARGV[2] =~ /\d{5}/) && !!(ARGV[3] =~ /\d{6}/)
+  alteration = enigma.encrypt(source, ARGV[2].to_s, ARGV[3].to_s)
+elsif !!(ARGV[2] =~ /\d{5}/)
+  alteration = enigma.encrypt(source, ARGV[2].to_s)
+else
+  alteration = enigma.encrypt(source)
+end
 
 enigma.write_file(alteration[:encryption])
 puts "Created '#{ARGV[1]}' with the key #{alteration[:key]} and date #{alteration[:date]}"
